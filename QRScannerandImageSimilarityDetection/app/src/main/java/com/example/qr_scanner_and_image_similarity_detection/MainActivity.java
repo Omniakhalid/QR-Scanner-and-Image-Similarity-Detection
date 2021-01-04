47f491ba25419689e44f49db671734974d45580d
AbanoOoub <40705922+AbanoOoub@users.noreply.github.com>
2021-01-04 19:15:37 +0200

    Project Creation

---
+++ QRScannerandImageSimilarityDetection/app/src/main/java/com/example/qr_scanner_and_image_similarity_detection/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.qr_scanner_and_image_similarity_detection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
