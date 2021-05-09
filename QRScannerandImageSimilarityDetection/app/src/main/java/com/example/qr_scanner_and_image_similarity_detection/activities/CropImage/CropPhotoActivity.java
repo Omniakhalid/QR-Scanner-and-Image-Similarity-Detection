d9dc15f0faf4b8a6517a71dd2104dc10e0ca4ca5
Adel Tharwat <45374169+AdelSarwat@users.noreply.github.com>
2021-05-09 18:24:26 +0200

    Add Crop Image Activity
    crop image manually in upload photo fragment

---
+++ QRScannerandImageSimilarityDetection/app/src/main/java/com/example/qr_scanner_and_image_similarity_detection/activities/CropImage/CropPhotoActivity.java
@@ -0,0 +1,79 @@
+package com.example.qr_scanner_and_image_similarity_detection.activities.CropImage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.example.qr_scanner_and_image_similarity_detection.R;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+public class CropPhotoActivity extends AppCompatActivity {
+
+    private ImageButton btBrowse, btReset;
+    private ImageView imageView;
+    private Button btn_upload_image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crop_photo);
+
+        Init();
+
+
+        btBrowse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startCrop();
+            }
+        });
+
+        btReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageView.setImageBitmap(null);
+            }
+        });
+
+        btn_upload_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void Init() {
+        btBrowse = findViewById(R.id.bt_borwse);
+        btReset = findViewById(R.id.bt_reset);
+        imageView = findViewById(R.id.image_view);
+        btn_upload_image = findViewById(R.id.btn_upload_cropedPhoto);
+    }
+    private void startCrop() {
+        CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .start(this);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                imageView.setImageURI(resultUri);
+                btn_upload_image.setVisibility(View.VISIBLE);
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
+    }
+}
